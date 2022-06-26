36877334a3bf7abaf66ab1e3a3cf4d16025af0b2
dallasmay <dmmay9@gmail.com>
2022-06-26 10:52:39 -0600

    completed step 6, getting cities

--- server/controller.js
+++ server/controller.js
@@ -244,5 +244,16 @@ module.exports = {
         )
         .then((dbRes) => res.status(200).send(dbRes[0]))
         .catch((err) => console.log(err));
+    },
+
+    getCities: (req, res) => {
+        sequelize.query(`SELECT cities.name AS city, cities.city_id,  cities.rating, countries.country_id, countries.name AS country
+                        FROM cities
+                        JOIN countries ON cities.country_id = countries.country_id`
+        )
+        .then((dbRes) => {
+            res.status(200).send(dbRes[0])
+        })           
+        .catch((err) => console.log(err));
     }
 }
